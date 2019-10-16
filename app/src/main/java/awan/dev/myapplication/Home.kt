79f7cdf99684517fc7280237bc9c81f084539eb2
awn <hermawans710@gmail.com>
2019-10-16 14:58:34 +0700

    first commit

--- app/src/main/java/awan/dev/myapplication/Home.kt
+++ app/src/main/java/awan/dev/myapplication/Home.kt
@@ -10,10 +10,6 @@ class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        Handler().postDelayed({
-            startActivity(Intent(this@Home, MainActivity::class.java))
-            finish()
-        },4000)
+        
     }
 }
